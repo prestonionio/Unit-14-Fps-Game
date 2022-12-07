50969b03b384870fd1b3ce0fadab9f2cd238f88c
prestonionio <703924@student.cityofbristol.ac.uk>
2022-12-07 14:44:12 +0000

    Commit 003

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,9 +19,19 @@ public class PlayerMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(x, 0, z);
+        animator.SetInteger("WeaponType_int", 1);
         transform.Translate(movement * speed * Time.deltaTime);
 
         animator.SetFloat("Speed_f", movement.magnitude);   
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetBool("Crouch_b", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.C))
+        {
+            animator.SetBool("Crouch_b", false);
+        }
     }
 
 }
